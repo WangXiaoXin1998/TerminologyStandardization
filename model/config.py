f759e7ead494944010acf5dd53029330baa7bd28
WangXiaoXin <49645010+WangXiaoXin1998@users.noreply.github.com>
2022-07-15 13:38:09 +0800

    Update config.py

--- model/config.py
+++ model/config.py
@@ -7,7 +7,7 @@ hyper = {
     "learning_rate": 1e-4,
     "gpu_id": 0,
     "bert": False,
-    "bert_path": 'hfl/chinese-macbert-base',
+    "bert_path": 'bert-base-chinese',
     "train_path": 'data/pre_train.csv',
     "valid_path": 'data/pre_val.csv',
     "code_path": 'data/code.csv'
